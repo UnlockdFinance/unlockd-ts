a32dc5ce3a88a153a62b7d860476db65c0ba89a8
vladmee <vladpresedinte@gmail.com>
2024-08-29 09:27:49 +0800

    update configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,45 @@
-import typescript from "@rollup/plugin-typescript";
-import json from '@rollup/plugin-json';
-import { importAssertionsPlugin } from 'rollup-plugin-import-assert';
-import { importAssertions } from 'acorn-import-assertions';
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+import { importAssertionsPlugin } from 'rollup-plugin-import-assert'
+import { importAssertions } from 'acorn-import-assertions'
 import { dts } from 'rollup-plugin-dts'
 
-import pkg from "./package.json";
+import pkg from './package.json'
 
 export default [
-    // browser-friendly UMD build
-    {
-        input: "src/index.ts",
-        output: {
-            name: "unlockTs",
-            file: pkg.browser,
-            format: "umd",
-        },
-        acornInjectPlugins: [importAssertions],
-
-        plugins: [
-            importAssertionsPlugin(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-            json(),
-        ],
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    external: ['axios', 'joi', 'viem', 'viem/chains'],
+    output: {
+      name: 'unlockTs',
+      file: pkg.browser,
+      format: 'umd',
+      globals: {
+        axios: 'axios',
+        joi: 'Joi',
+        viem: 'viem',
+        'viem/chains': 'chains'
+      }
     },
-    {
-        input: "src/index.ts",
-        output: [
-            { file: pkg.main, format: "cjs" },
-            { file: pkg.module, format: "es" },
-        ],
-        acornInjectPlugins: [importAssertions],
+    acornInjectPlugins: [importAssertions],
 
-        plugins: [
-            importAssertionsPlugin(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-            json(),
-        ],
-    },
-    {
-        input: "./build/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "es" }],
-        plugins: [dts()],
-    },
+    plugins: [importAssertionsPlugin(), typescript({ tsconfig: './tsconfig.json' }), json()]
+  },
+  {
+    input: 'src/index.ts',
+    external: ['axios', 'joi', 'viem', 'viem/chains'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    acornInjectPlugins: [importAssertions],
 
-];
+    plugins: [importAssertionsPlugin(), typescript({ tsconfig: './tsconfig.json' }), json()]
+  },
+  {
+    input: './src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()]
+  }
+]
