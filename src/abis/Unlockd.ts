d2d375e79212d361dbc139e4f9baf7234064d4a5
vladmee <vladpresedinte@gmail.com>
2024-09-02 10:18:25 +0800

    fix scripts

--- src/abis/Unlockd.ts
+++ src/abis/Unlockd.ts
@@ -1,5 +1,10 @@
 const unlockdInterface: string[] = [
   'function dispatch()',
+  'function moduleIdToImplementation(uint256 moduleId) view returns (address)',
+  'function moduleIdToProxy(uint256 moduleId) view returns (address)',
+  // events
+  'event InstallerInstallModule(uint256 indexed moduleId, address indexed moduleImpl, bytes32 moduleVersion)',
+  'event ProxyCreated(address indexed proxy, uint256 moduleId)',
   // errors
   'error InvalidModule()'
 ] as const
